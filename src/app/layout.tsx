06d9acd8644c3e503c83e1c72db2ecd09c8a5521
he2e2 <dddd6368@naver.com>
2024-10-08 11:50:07 +0900

    chore: Add pwa setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import "./globals.scss";
+
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   title: "meta-log",
   description: "seo-friendly-blog",
+  manifest: "/manifest.json",
+  icons: [{ rel: "icon", url: "/icons/192.png", sizes: "192x192" }],
 };
 
 export default function RootLayout({
